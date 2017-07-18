fd30673a60476917b5d614ffa4445d295f113940
Cameron Jensen <jensenseanc@gmail.com>
2017-07-18 06:26:58 -0700

    add maps to display one marker

--- app/src/main/java/com/seacam/fotofind/activity/MainActivity.java
+++ app/src/main/java/com/seacam/fotofind/activity/MainActivity.java
@@ -2,15 +2,22 @@ package com.seacam.fotofind.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import info.androidhive.locationapi.R;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap map;
@@ -19,12 +26,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map).getMap());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+//        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map).getMap);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
     public boolean onCreateOptionsMenu (Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 }
