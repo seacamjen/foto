95ff2059e479d3e21aa429a087fc904920823143
Cameron Jensen <jensenseanc@gmail.com>
2017-07-18 06:27:50 -0700

    remove bad code

--- app/src/main/java/com/seacam/fotofind/activity/MainActivity.java
+++ app/src/main/java/com/seacam/fotofind/activity/MainActivity.java
@@ -1,14 +1,11 @@
 package com.seacam.fotofind.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -18,9 +15,6 @@ import info.androidhive.locationapi.R;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    static final LatLng KIEL = new LatLng(53.551, 9.993);
-    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +24,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
-//        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map).getMap);
     }
 
     @Override
