598ec4f8af9817b294ee0a79270e5aa369c4d482
Cameron Jensen <jensenseanc@gmail.com>
2017-07-17 20:57:40 -0700

    add all tutorial except api key

--- app/src/main/java/com/seacam/fotofind/activity/MainActivity.java
+++ app/src/main/java/com/seacam/fotofind/activity/MainActivity.java
@@ -1,15 +1,30 @@
 package com.seacam.fotofind.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 import info.androidhive.locationapi.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map).getMap());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
