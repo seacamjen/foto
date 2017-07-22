3d4406b620a0f1bf886daf170f7af37c39174642
Cameron Jensen <jensenseanc@gmail.com>
2017-07-22 11:43:08 -0700

    moved location to savefoto from camera activity

--- app/src/main/java/com/seacam/fotofind/activity/CameraActivity.java
+++ app/src/main/java/com/seacam/fotofind/activity/CameraActivity.java
@@ -3,44 +3,21 @@ package com.seacam.fotofind.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.location.Location;
 import android.provider.MediaStore;
 import android.support.v4.view.GestureDetectorCompat;
 import android.os.Bundle;
 import android.util.Base64;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 
 import java.io.ByteArrayOutputStream;
 
 import info.androidhive.locationapi.R;
 
-public class CameraActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class CameraActivity extends Activity {
     private static final int REQUEST_IMAGE_CAPTURE = 111;
     private GestureDetectorCompat mDetector;
     private String imageToSave;
-    private String latitude;
-    private String longitude;
-    private String time;
-
-    private static final String TAG = CameraActivity.class.getSimpleName();
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
-    private Location mLastLocation;
-
-    private GoogleApiClient mGoogleApiClient;
-
-    private TextView lblLocation;
-    private Button btnShowLocation, btnStartLocationUpdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,85 +26,7 @@ public class CameraActivity extends Activity implements GoogleApiClient.Connecti
         setContentView(R.layout.activity_camera);
         mDetector = new GestureDetectorCompat(this, new MyGestureListener());
 
-        lblLocation = (TextView) findViewById(R.id.lblLocation);
-        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
-        btnStartLocationUpdates = (Button) findViewById(R.id.btnLocationUpdates);
-
-        if (checkPlayServices()) {
-            buildGoogleApiClient();
-        }
     }
-    //begin location
-
-
-    private void displayLocation() {
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
-        if (mLastLocation != null) {
-            latitude = Double.toString(mLastLocation.getLatitude());
-            longitude = Double.toString(mLastLocation.getLongitude());
-            time = Long.toString(mLastLocation.getTime());
-
-            lblLocation.setText(latitude + ", " + longitude + ", " + time);
-
-        } else {
-
-            lblLocation.setText("(Couldn't get the location. Make sure location is enabled on the device)");
-        }
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API).build();
-    }
-
-    protected boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
-
-            } else {
-                Toast.makeText(getApplicationContext(), "This device is not supported.", Toast.LENGTH_LONG).show();
-                finish();
-            }
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        checkPlayServices();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
-    }
-
-    @Override
-    public void onConnected(Bundle arg0) {
-        displayLocation();
-    }
-
-    @Override
-    public void onConnectionSuspended(int arg0) {
-        mGoogleApiClient.connect();
-    }
-
-    //end of location
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -157,12 +56,8 @@ public class CameraActivity extends Activity implements GoogleApiClient.Connecti
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             encodeBitmapAndSaveToFirebase(imageBitmap);
-            displayLocation();
 
             Intent intent = new Intent(CameraActivity.this, SaveFoto.class);
-            intent.putExtra("latitude", latitude);
-            intent.putExtra("longitude", longitude);
-            intent.putExtra("time", time);
             intent.putExtra("foto", imageToSave);
             startActivity(intent);
         }
--- app/src/main/java/com/seacam/fotofind/activity/SaveFoto.java
+++ app/src/main/java/com/seacam/fotofind/activity/SaveFoto.java
@@ -1,20 +1,26 @@
 package com.seacam.fotofind.activity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.seacam.fotofind.models.Fotos;
 
 import info.androidhive.locationapi.R;
 
-public class SaveFoto extends AppCompatActivity {
+public class SaveFoto extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private DatabaseReference mActiveRef;
 
     private GestureDetectorCompat mDetector;
@@ -23,6 +29,12 @@ public class SaveFoto extends AppCompatActivity {
     private String longitude;
     private String time;
     private String foto;
+
+    private static final String TAG = SaveFoto.class.getSimpleName();
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private Location mLastLocation;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +46,84 @@ public class SaveFoto extends AppCompatActivity {
         setContentView(R.layout.activity_save_foto);
         mDetector = new GestureDetectorCompat(this, new MyGestureListener());
 
+        if (checkPlayServices()) {
+            buildGoogleApiClient();
+        }
+
         Intent intent = getIntent();
         latitude = intent.getStringExtra("latitude");
         longitude = intent.getStringExtra("longitude");
         time = intent.getStringExtra("time");
         foto = intent.getStringExtra("foto");
     }
+
+    //begin location
+
+
+    private void displayLocation() {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            latitude = Double.toString(mLastLocation.getLatitude());
+            longitude = Double.toString(mLastLocation.getLongitude());
+            time = Long.toString(mLastLocation.getTime());
+        } else {
+
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    protected boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+
+            } else {
+                Toast.makeText(getApplicationContext(), "This device is not supported.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        displayLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
+    }
+
+    //end of location
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
