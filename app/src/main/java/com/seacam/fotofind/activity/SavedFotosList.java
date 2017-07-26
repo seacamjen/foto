b062dc1bb284a8075de3184f74b72b9cec96c44a
Cameron Jensen <jensenseanc@gmail.com>
2017-07-25 17:02:33 -0700

    add name for list activity, no images showing

--- app/src/main/java/com/seacam/fotofind/activity/SavedFotosList.java
+++ app/src/main/java/com/seacam/fotofind/activity/SavedFotosList.java
@@ -2,7 +2,9 @@ package com.seacam.fotofind.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
@@ -26,17 +28,27 @@ public class SavedFotosList extends AppCompatActivity {
         setContentView(R.layout.activity_saved_fotos_list);
         ButterKnife.bind(this);
 
-        mFotosRef = FirebaseDatabase.getInstance().getReference("photos");
+        mFotosRef = FirebaseDatabase.getInstance().getReference("photos").child("image");
+        Log.i("Firebase", mFotosRef.toString());
 
         setUpFirebaseAdapter();
     }
 
     private void setUpFirebaseAdapter() {
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Fotos, FirebaseFotoViewHolder>(Fotos.class, R.layout.) {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Fotos, FirebaseFotoViewHolder>(Fotos.class, R.layout.foto_list_item, FirebaseFotoViewHolder.class, mFotosRef) {
             @Override
-            protected void populateViewHolder(RecyclerView.ViewHolder viewHolder, Object model, int position) {
-
+            protected void populateViewHolder(FirebaseFotoViewHolder viewHolder, Fotos model, int position) {
+                viewHolder.bindFoto(model);
             }
         };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 }
