49df61bc01561b33595c154bef0be08d27435edc
Cameron Jensen <jensenseanc@gmail.com>
2017-07-26 06:20:47 -0700

    optimized imports removing unused

--- app/src/main/java/com/seacam/fotofind/FirebaseFotoViewHolder.java
+++ app/src/main/java/com/seacam/fotofind/FirebaseFotoViewHolder.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.seacam.fotofind.models.Fotos;
-import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 
--- app/src/main/java/com/seacam/fotofind/activity/CameraActivity.java
+++ app/src/main/java/com/seacam/fotofind/activity/CameraActivity.java
@@ -2,9 +2,9 @@ package com.seacam.fotofind.activity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.view.GestureDetectorCompat;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.view.GestureDetector;
