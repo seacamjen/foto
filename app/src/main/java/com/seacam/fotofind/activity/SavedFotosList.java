da110d15f4f2d53f68398729d55cf5604a09b267
Cameron Jensen <jensenseanc@gmail.com>
2017-07-26 15:50:13 -0700

    add menu option for saved fotos list and navigation

--- app/src/main/java/com/seacam/fotofind/activity/SavedFotosList.java
+++ app/src/main/java/com/seacam/fotofind/activity/SavedFotosList.java
@@ -1,10 +1,14 @@
 package com.seacam.fotofind.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DataSnapshot;
@@ -63,6 +67,30 @@ public class SavedFotosList extends AppCompatActivity {
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
         mRecyclerView.setAdapter(mFirebaseAdapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_saved_fotos_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_map) {
+            Intent intent = new Intent(SavedFotosList.this, MapActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_foto) {
+            Intent intent = new Intent(SavedFotosList.this, CameraActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onDestroy() {
