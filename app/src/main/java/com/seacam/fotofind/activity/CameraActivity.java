c41841e10620f91a87500afd1f35684c51cfc340
Cameron Jensen <jensenseanc@gmail.com>
2017-07-26 06:37:32 -0700

    optimized imports

--- app/src/main/java/com/seacam/fotofind/activity/CameraActivity.java
+++ app/src/main/java/com/seacam/fotofind/activity/CameraActivity.java
@@ -4,14 +4,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
-import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
