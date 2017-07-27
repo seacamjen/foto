8d8b78129599678d31680c36d6af13faeae5e60c
Cameron Jensen <jensenseanc@gmail.com>
2017-07-27 07:22:06 -0700

    add create new user method to create account java

--- app/src/main/java/com/seacam/fotofind/activity/CreateAccount.java
+++ app/src/main/java/com/seacam/fotofind/activity/CreateAccount.java
@@ -1,18 +1,30 @@
 package com.seacam.fotofind.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import info.androidhive.locationapi.R;
 
 public class CreateAccount extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = CreateAccount.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+
     @Bind(R.id.nameCreateAccount) EditText mName;
     @Bind(R.id.emailCreateAccount) EditText mEmail;
     @Bind(R.id.passwordCreateAccount) EditText mPassword;
@@ -25,6 +37,8 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mCreateButton.setOnClickListener(this);
         mLogin.setOnClickListener(this);
@@ -41,5 +55,24 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
         if (view == mCreateButton) {
             createNewUser();
         }
+    }
+
+    private void createNewUser() {
+        final String name = mName.getText().toString().trim();
+        final String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String confirmPassword = mConfirmPassword.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(CreateAccount.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
