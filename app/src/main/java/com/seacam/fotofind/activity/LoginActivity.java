26651890261f2121d728f16288537a9cdd3ff577
Cameron Jensen <jensenseanc@gmail.com>
2017-07-27 06:47:27 -0700

    add bind to move from login to create account

--- app/src/main/java/com/seacam/fotofind/activity/LoginActivity.java
+++ app/src/main/java/com/seacam/fotofind/activity/LoginActivity.java
@@ -1,15 +1,32 @@
 package com.seacam.fotofind.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import info.androidhive.locationapi.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccount.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
