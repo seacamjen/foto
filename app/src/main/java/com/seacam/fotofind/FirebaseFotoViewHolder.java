b98ff2cdb3583dbdafef6a990a6962494cbf8053
Cameron Jensen <jensenseanc@gmail.com>
2017-08-20 10:56:26 -0700

    add onClick view for viewHolder

--- app/src/main/java/com/seacam/fotofind/FirebaseFotoViewHolder.java
+++ app/src/main/java/com/seacam/fotofind/FirebaseFotoViewHolder.java
@@ -15,6 +15,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.seacam.fotofind.activity.ShowFoto;
 import com.seacam.fotofind.activity.SingleImageFragment;
@@ -35,6 +36,7 @@ public class FirebaseFotoViewHolder extends RecyclerView.ViewHolder implements V
     View mView;
     Context mContext;
     String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    String push;
 
     public FirebaseFotoViewHolder(View itemView) {
         super(itemView);
@@ -59,23 +61,22 @@ public class FirebaseFotoViewHolder extends RecyclerView.ViewHolder implements V
     public void onClick(View v) {
         final ArrayList<Fotos> fotoslist = new ArrayList<>();
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(userId);
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     fotoslist.add(snapshot.getValue(Fotos.class));
                 }
-
                 int itemPosition = getLayoutPosition();
 
                 Log.i("THIS IS THE POSITION", String.valueOf(itemPosition));
 
-//                Intent intent = new Intent(mContext, SingleImageFragment.class);
-//                intent.putExtra("position", itemPosition + "");
-//                intent.putExtra("fotos", Parcels.wrap(fotoslist));
-//
-//                mContext.startActivity(intent);
+                Intent intent = new Intent(mContext, SingleImageFragment.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("fotos", Parcels.wrap(fotoslist));
+
+                mContext.startActivity(intent);
             }
 
             @Override
