9888b197df5cd00cc5c35362d3bd67f60a285a48
Cameron Jensen <jensenseanc@gmail.com>
2017-08-21 16:28:18 -0700

    add x mark image, remove all code from viewholder onclick method

--- app/src/main/java/com/seacam/fotofind/FirebaseFotoViewHolder.java
+++ app/src/main/java/com/seacam/fotofind/FirebaseFotoViewHolder.java
@@ -11,10 +11,12 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.seacam.fotofind.models.Fotos;
 
@@ -53,32 +55,7 @@ public class FirebaseFotoViewHolder extends RecyclerView.ViewHolder implements V
 
     @Override
     public void onClick(View v) {
-        final ArrayList<Fotos> fotoslist = new ArrayList<>();
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(userId);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    fotoslist.add(snapshot.getValue(Fotos.class));
-                }
-
-                int itemPosition = getLayoutPosition();
-
-                Log.i("THIS IS THE POSITION", String.valueOf(itemPosition));
-
-//                Intent intent = new Intent(mContext, ImageDetails.class);
-//                intent.putExtra("position", itemPosition + "");
-//                intent.putExtra("fotos", Parcels.wrap(fotoslist));
-//
-//                mContext.startActivity(intent);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
     }
 
     public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
