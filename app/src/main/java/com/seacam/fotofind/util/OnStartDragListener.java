8f941a0521fe7de7fba4468d3eaa83f73189c394
Cameron Jensen <jensenseanc@gmail.com>
2017-08-22 10:39:51 -0700

    add OnStartDragListener interface

---
+++ app/src/main/java/com/seacam/fotofind/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.seacam.fotofind.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by jensensc on 8/22/17.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
