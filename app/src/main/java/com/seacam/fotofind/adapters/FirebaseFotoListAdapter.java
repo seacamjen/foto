f2fe46ac315b23ebc5e2bae2c09422af8540766e
Cameron Jensen <jensenseanc@gmail.com>
2017-08-22 12:17:50 -0700

    remove all reference to reassigning index in database

--- app/src/main/java/com/seacam/fotofind/adapters/FirebaseFotoListAdapter.java
+++ app/src/main/java/com/seacam/fotofind/adapters/FirebaseFotoListAdapter.java
@@ -6,12 +6,18 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.seacam.fotofind.FirebaseFotoViewHolder;
 import com.seacam.fotofind.models.Fotos;
 import com.seacam.fotofind.util.ItemTouchHelperAdapter;
 import com.seacam.fotofind.util.OnStartDragListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by jensensc on 8/22/17.
@@ -21,12 +27,41 @@ public class FirebaseFotoListAdapter extends FirebaseRecyclerAdapter<Fotos, Fire
     private DatabaseReference mRef;
     private OnStartDragListener mOnStartDragListener;
     private Context mContext;
+    private ChildEventListener mChildEventListener;
+    private ArrayList<Fotos> mFotos = new ArrayList<>();
 
     public FirebaseFotoListAdapter(Class<Fotos> modelClass, int modelLayout, Class<FirebaseFotoViewHolder> viewHolderClass, Query ref, OnStartDragListener onStartDragListener, Context context) {
         super(modelClass, modelLayout, viewHolderClass, ref);
         mRef = ref.getRef();
         mOnStartDragListener = onStartDragListener;
         mContext = context;
+
+//        mChildEventListener = mRef.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                mFotos.add(dataSnapshot.getValue(Fotos.class));
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
     }
 
     @Override
@@ -45,12 +80,30 @@ public class FirebaseFotoListAdapter extends FirebaseRecyclerAdapter<Fotos, Fire
 
     @Override
     public boolean onItemMove(int fromPosition, int toPoisition) {
+//        Collections.swap(mFotos, fromPosition, toPoisition);
         notifyItemMoved(fromPosition, toPoisition);
         return false;
     }
 
     @Override
     public void onItemDismiss(int position) {
+//        mFotos.remove(position);
         getRef(position).removeValue();
     }
+
+//    private void setIndexInFirebase() {
+//        for (Fotos foto : mFotos) {
+//            int index = mFotos.indexOf(foto);
+//            DatabaseReference ref = getRef(index);
+//            foto.setIndex(Integer.toString(index));
+//            ref.setValue(foto);
+//        }
+//    }
+//
+//    @Override
+//    public void cleanup() {
+//        super.cleanup();
+//        setIndexInFirebase();
+//        mRef.removeEventListener(mChildEventListener);
+//    }
 }
