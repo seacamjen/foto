91e77bce697ddddfd7878c936caf044d0f13caea
Cameron Jensen <jensenseanc@gmail.com>
2017-08-25 10:08:45 -0700

    add tutorial layout and activity

---
+++ app/src/main/java/com/seacam/fotofind/activity/Tutorial.java
@@ -0,0 +1,15 @@
+package com.seacam.fotofind.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.androidhive.locationapi.R;
+
+public class Tutorial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+}
